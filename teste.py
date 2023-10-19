1f560118f523e7e31a1495b15af821f7dced672b
Leonardo Stempfle de Azevedo <leosta132003@gmail.com>
2023-10-19 19:53:56 -0300

    MENSAGEM 2

--- teste.py
+++ teste.py
@@ -1,7 +1,15 @@
+### Edição Leo 1
 print("Teste")
 # Comentário
 
+### Edição Walkiria 1
 def calc_sum(x,y):
     return x+y
     
 print(calc_sum(1,1))
+
+### Edição Leo 2
+def quadrado(x)
+    return x^2
+
+print(quadrado(2))
