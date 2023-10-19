f011539110d60d0749cfdb2da34cb773285c1df3
WalkiriaResende <walkiriaresende@gmail.com>
2023-10-19 20:09:33 -0300

    revertendo2

--- teste.py
+++ teste.py
@@ -1,1 +1,21 @@
+### Edição Walkiria 1
+def calc_sum(x,y):
+    return x+y
+    
+print(calc_sum(1,1))
 
+### Edição Leo 2
+def quadrado_(x)
+    z = x+x
+    return x^2
+
+print(quadrado(2))
+
+
+print('1, 2, 3')
+
+soma()
+
+# Conflito resolvido!
+
+# o conflito aconteceu quando eu subi o arquivo modificado, a pessoa modificou e quando tentou baixar e subir novamente deu conflito
