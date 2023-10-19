914f941bdd80afad585af4f93416fabaaf6f9e93
Leonardo Stempfle de Azevedo <leosta132003@gmail.com>
2023-10-19 19:59:44 -0300

    MENSAGEM 3: É PARA DAR CONFLITO

--- teste.py
+++ teste.py
@@ -1,16 +1,1 @@
-### Edição Leo 1
-print("Teste")
-# Comentário
 
-### Edição Walkiria 1
-def calc_sum(x,y):
-    return x+y
-    
-print(calc_sum(1,1))
-
-### Edição Leo 2
-def quadrado_(x)
-    z = x+x
-    return x^2
-
-print(quadrado(2))
